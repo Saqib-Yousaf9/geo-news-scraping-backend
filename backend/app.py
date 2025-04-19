27f1a6a8e9ae3a864eb08816479c4483e9cf25a6
Saqib Yousaf <114312210+Saqib-Yousaf9@users.noreply.github.com>
2025-04-19 05:27:13 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -135,4 +135,4 @@ def visualize_nap():
     return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
