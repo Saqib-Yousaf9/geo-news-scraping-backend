acc1be79bd41a489ad08a4b19bd60d940e5d7f62
Saqib Yousaf <114312210+Saqib-Yousaf9@users.noreply.github.com>
2025-04-20 19:39:05 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -141,3 +141,6 @@ def visualize_nap():
     img_io.seek(0)
 
     return send_file(img_io, mimetype='image/png')
+    if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
